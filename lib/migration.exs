b2c35110100a6c48f7fd880fc47d0bc1e083d8a3
mithereal <mithereal@gmail.com>
2023-07-21 20:23:45 -0700

    .

--- lib/migration.exs
+++ lib/migration.exs
@@ -4,7 +4,19 @@ defmodule ExDbmigrate.Util.Migration do
     def change do
 
       alter table(@schema.table) do
-#        generate_references(@schema)
+        for(l <- @schema.links)do
+          migration_module =
+            String.split(l.references.ref_table, "_")
+            |> Enum.map(fn x -> String.capitalize(x) end)
+            |> Enum.join("")
+
+          case l.references.type do
+            :belongs_to ->  belongs_to(l.references.ref_table, migration_module)
+            :many_to_many -> many_to_many(l.references.ref_table, migration_module)
+            :has_many -> has_many(l.references.ref_table, migration_module)
+            _ ->  has_one(l.references.ref_table, migration_module)
+          end
+        end
 
       end
 
