f0d2f7e69db56384624ff2c69b6324ca5e4e2e9b
mithereal <mithereal@gmail.com>
2025-09-27 13:53:19 -0700

    update coveralls

--- mix.exs
+++ mix.exs
@@ -15,7 +15,15 @@ defmodule ExDbmigrate.MixProject do
       docs: docs(),
       aliases: aliases(),
       name: "ex_dbmigrate",
-      source_url: @source_url
+      source_url: @source_url,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
     ]
   end
 
